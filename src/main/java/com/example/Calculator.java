89db872f0b646e76755ad9ea0295667f8fe4a9ee
ngocnghia81 <ngocnghia.workspace@gmail.com>
2025-05-24 22:16:59 +0700

    test: intentionally break add() for CI failure test

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -4,19 +4,21 @@ package com.example;
  * A simple calculator class for basic operations
  */
 public class Calculator {
-    
+
     /**
      * Add two numbers
+     * 
      * @param a first number
      * @param b second number
      * @return sum of a and b
      */
     public int add(int a, int b) {
-        return a + b;
+        return a + b + 2;
     }
-    
+
     /**
      * Subtract second number from first
+     * 
      * @param a first number
      * @param b second number
      * @return difference between a and b
@@ -24,9 +26,10 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
-    
+
     /**
      * Multiply two numbers
+     * 
      * @param a first number
      * @param b second number
      * @return product of a and b
@@ -34,9 +37,10 @@ public class Calculator {
     public int multiply(int a, int b) {
         return a * b;
     }
-    
+
     /**
      * Divide first number by second
+     * 
      * @param a first number
      * @param b second number
      * @return result of division
@@ -48,4 +52,4 @@ public class Calculator {
         }
         return a / b;
     }
-} 
+}
